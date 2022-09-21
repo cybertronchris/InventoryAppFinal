a492e72e37e8c2a6e4f2ca5101af8ccda83146aa
cybertronchris <103653014+cybertronchris@users.noreply.github.com>
2022-09-20 22:56:50 -0500

    server

---
+++ server/routes/allcars.js
@@ -0,0 +1,84 @@
+const express = require("express");
+ 
+// recordRoutes is an instance of the express router.
+// We use it to define our routes.
+// The router will be added as a middleware and will take control of requests starting with path /record.
+const allcarsRoutes = express.Router();
+ 
+// This will help us connect to the database
+const dbo = require("../db/connectionTOdatabase");
+ 
+// This help convert the id from string to ObjectId for the _id.
+const ObjectId = require("mongodb").ObjectId;
+ 
+ 
+// This section will help you get a list of all the records.
+allcarsRoutes.route("/allcars").get(function (req, res) {
+ let db_connect = dbo.getDb("cars");
+ db_connect
+   .collection("cars")
+   .find({})
+   .toArray(function (err, result) {
+     if (err) throw err;
+     res.json(result);
+   });
+});
+ 
+// This section will help you get a single record by id
+allcarsRoutes.route("/allcars/:id").get(function (req, res) {
+ let db_connect = dbo.getDb();
+ let myquery = { _id: ObjectId(req.params.id) };
+ db_connect
+   .collection("cars")
+   .findOne(myquery, function (err, result) {
+     if (err) throw err;
+     res.json(result);
+   });
+});
+ 
+// This section will help you create a new record.
+allcarsRoutes.route("/allcars/add").post(function (req, response) {
+ let db_connect = dbo.getDb();
+ let myobj = {
+   name: req.body.name,
+   position: req.body.position,
+   level: req.body.level,
+ };
+ db_connect.collection("cars").insertOne(myobj, function (err, res) {
+   if (err) throw err;
+   response.json(res);
+ });
+});
+ 
+// This section will help you update a record by id.
+allcarsRoutes.route("/update/:id").post(function (req, response) {
+ let db_connect = dbo.getDb();
+ let myquery = { _id: ObjectId(req.params.id) };
+ let newvalues = {
+   $set: {
+     name: req.body.name,
+     position: req.body.position,
+     level: req.body.level,
+   },
+ };
+ db_connect
+   .collection("cars")
+   .updateOne(myquery, newvalues, function (err, res) {
+     if (err) throw err;
+     console.log("1 document updated");
+     response.json(res);
+   });
+});
+ 
+// This section will help you delete a record
+allcarsRoutes.route("/:id").delete((req, response) => {
+ let db_connect = dbo.getDb();
+ let myquery = { _id: ObjectId(req.params.id) };
+ db_connect.collection("cars").deleteOne(myquery, function (err, obj) {
+   if (err) throw err;
+   console.log("1 document deleted");
+   response.json(obj);
+ });
+});
+ 
+module.exports = allcarsRoutes;
