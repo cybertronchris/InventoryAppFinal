0b98ced3d32eb83becd70b5838d0032520e2dc1d
Beck <rsturn29@gmail.com>
2022-10-20 21:57:33 -0500

    new

--- server/routes/cars.routes.js
+++ server/routes/cars.routes.js
@@ -12,7 +12,7 @@ module.exports = function(app) {
   });
 
 
-  app.use('/cars', cars.routes);
+
 
 
 
--- server/server.js
+++ server/server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-<<<<<<< HEAD
 const cors = require("cors");
 //added line below
 const bodyParser = require('body-parser');
@@ -8,22 +7,10 @@ const db = require("./models");
 const Role = db.role;
 var corsOptions = {
   origin: "http://localhost:3000"
-=======
-
-const cors = require("cors");
-const bodyParser = require('body-parser');
-const app = express();
-
-const db = require("./models");
-const Role = db.role;
-var corsOptions = {
-  origin: "http://localhost:5000"
->>>>>>> 9f8b111abd72170e207b80da02a7a11e065ca888
 };
 
 app.use(cors(corsOptions));
 
-<<<<<<< HEAD
 // parse requests of content-type - application/json
 app.use(express.json());
 //added 2 lines below
@@ -73,71 +60,6 @@ app.get("/", (req, res) => {
   console.log("added 'user' to roles collection");
 });
 
-=======
-app.use(cors(corsOptions));
-app.use(express.json())
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(express.urlencoded({ extended: true}));
-app.get("/",(req, res) => {
-  res.json({ message: 'Welcome'});
-});
-
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
-  );
-  next();
-});
-
-require('./routes/auth.routes') (app)
-require('./routes/user.routes')(app);
-
-
-
-
-
-
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
-  console.log(`Server is running on port: ${port}`);
-});
-
-db.mongoose
-  .connect(
-`mongodb+srv://dbuser:Password1!@cluster0.zuxhij4.mongodb.net/?retryWrites=true&w=majority`,
-{    useNewUrlParser: true,
-      useUnifiedTopology: true
-})
-    .then(() => {
-      console.log("Successfully connected to MongoDB.");
-      initial();
-    })
-    .catch(err => {
-      console.error("Connection error", err);
-      process.exit();
-  });
-
-  function initial() {
-    Role.estimatedDocumentCount((err, count) => {
-      if (!err && count === 0) {
-        new Role({
-          name: "user"
-        }).save(err => {
-          if (err) {
-            console.log("error", err);
-          }
-  
-  console.log("added 'user' to roles collection");
-});
-
->>>>>>> 9f8b111abd72170e207b80da02a7a11e065ca888
 new Role({
   name: "moderator"
 }).save(err => {
@@ -159,8 +81,4 @@ new Role({
 });
 }
 });
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 9f8b111abd72170e207b80da02a7a11e065ca888
